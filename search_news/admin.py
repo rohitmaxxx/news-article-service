f7db62822ef6037c8e1023a8f663a1041dc7a754
rohit bharti <rohit.bharti@binmile.com>
2023-07-17 04:10:28 +0530

    undo changes

--- search_news/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import newsArticle, searchHistory, newsUser
-
-# Register your models here.
-admin.site.register((newsArticle, searchHistory, newsUser))
